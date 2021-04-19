6a1ac7fb6cc96f32a580b9899d30f8d5e87b7bbe
tsuruken <tsurumotokentarou@gmail.com>
2021-04-19 18:01:33 +0900

    adjust layout

--- PinterestLikeUI/TermLabel.swift
+++ PinterestLikeUI/TermLabel.swift
@@ -26,50 +26,49 @@ struct TermLabel: View {
         let width: CGFloat = UIScreen.main.bounds.width / 2 - (xMargin * CGFloat(columnCount+1)) / 2
         let columns: [GridItem] = Array(repeating: .init(.fixed(width), spacing: xMargin), count: columnCount)
         
-        return LazyVGrid(columns: columns, spacing: xMargin) {
-            ForEach(datas) { data in
-                ItemView(content: data.content, width: width)
-                    .frame(minHeight: 150)
-                    .frame(maxHeight: 400)
+        return ZStack(alignment: .topLeading) {
+            LazyVGrid(columns: columns, spacing: xMargin) {
+                ForEach(datas) { data in
+                    GeometryReader { geometryReader in
+                        generateContent(in: geometryReader, data: data, width: width)
+                            .frame(minHeight: 150)
+                            .frame(maxHeight: 400)
+                    }
+                }
             }
         }
     }
     
-    private func generateContent(in g: GeometryProxy) -> some View {
-        var width = CGFloat.zero
-        var height = CGFloat.zero
+    private func generateContent(in g: GeometryProxy, data: CustomData, width: CGFloat) -> some View {
+        var tempWidth = CGFloat.zero
+        var tempHeight = CGFloat.zero
         
-        return ZStack(alignment: .topLeading) {
-            ForEach(datas) { data in
-                ItemView(content: data.content, width: 200)
-                    .padding([.horizontal, .vertical], 0)
-                    .alignmentGuide(.leading, computeValue: { d in
-                        if (abs(width - d.width) > g.size.width)
-                        {
-                            width = 0
-                            height -= d.height
-                        }
-                        
-                        let result = width
-                        
-                        if data.id == datas.last?.id {
-                            width = 0
-                        }
-                        else {
-                            width -= d.width
-                        }
-                        
-                        return result
-                    })
-                    .alignmentGuide(.top, computeValue: {d in
-                        let result = height
-                        if data.id == datas.last?.id {
-                            height = 0 // last item
-                        }
-                        return result
-                    })
-            }
-        }
+        return ItemView(content: data.content, width: width)
+            .alignmentGuide(.leading, computeValue: { d in
+                if (abs(width - d.width) > g.size.width)
+                {
+                    tempWidth = 0
+                    tempHeight -= d.height
+                }
+                
+                let result = width
+                
+                if data.id == datas.last?.id {
+                    tempWidth = 0
+                }
+                else {
+                    tempWidth -= d.width
+                }
+                
+                return result
+            })
+            .alignmentGuide(.top, computeValue: {d in
+                let result = tempHeight
+                if data.id == datas.last?.id {
+                    tempHeight = 0 // last item
+                }
+                return result
+            })
     }
 }
 
