46b0e6bfd579fed8e0821f8bb80aed55f1c4366d
tsuruken <tsurumotokentarou@gmail.com>
2021-04-19 17:07:16 +0900

    Initial Commit

---
+++ PinterestLikeUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PinterestLikeUI
+//
+//  Created by 鶴本賢太朗 on 2021/04/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PinterestLikeUI/PinterestLikeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterestLikeUIApp.swift
+//  PinterestLikeUI
+//
+//  Created by 鶴本賢太朗 on 2021/04/19.
+//
+
+import SwiftUI
+
+@main
+struct PinterestLikeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
